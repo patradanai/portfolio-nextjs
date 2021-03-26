855a561dee5ee3eff58d6fa8d9a8fa7aff000c79
unknown <bloody_jojo@hotmail.com>
2021-03-26 16:41:38 +0700

    add function parserContext

--- functions/functions.js
+++ functions/functions.js
@@ -1,1 +1,28 @@
-export const ParsePosts = () => {};
+export const ParsePosts = (content) => {
+  let newArrayContents = null;
+  if (content) {
+    return newArrayContents;
+  }
+
+  const itemContents = content?.items;
+  const newContents = {};
+  newArrayContents = [];
+
+  // Loop Parser
+  itemContents.map((val) => {
+    newContents.title = val?.fields?.title;
+    newContents.author = val?.fields?.authorName;
+    newContents.category = val?.fields?.categoryName;
+    newContents.date = val?.fields?.date;
+    newContents.slug = val?.fields?.slug;
+    newContents.image = {
+      url: val?.fields?.image?.url,
+      alt: val?.fields?.image?.label,
+    };
+
+    // Add to Array
+    newArrayContents.push(newContents);
+  });
+
+  return newArrayContents;
+};
